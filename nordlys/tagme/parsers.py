73229f27ffe1ee61fad2ca260b7d57bbb3c8f27e
tc <taylorcassidy64@gmail.com>
2018-07-17 17:35:30 -0400

    started working on alternate parsers

---
+++ nordlys/tagme/parsers.py
@@ -0,0 +1,72 @@
+"""
+Query parsers for finding phrases that are not part of wikipedia anchor index.
+"""
+
+from spacy.matcher import Matcher
+import textacy
+
+
+#nlp = spacy.load('en')
+
+def get_matcher(pattern_name, patterns, nlp):
+    m = Matcher(nlp.vocab)
+    for p in patterns:
+        m.add(pattern_name, None, p)
+
+    return m
+
+
+def get_start_end_text_from_span(span):
+    start = span.start_char
+    end = span.end_char
+    mention_txt = span.text
+    mention = {"start": start, "end": end, "text": mention_txt}
+
+    return mention
+
+
+def get_simple_adj_n(nlp):
+    pattern = [{'TAG': 'JJ'}, {'POS': 'NOUN'}]
+    patterns = [pattern]
+    matcher = PosTagPatternMatcher(patterns, nlp, name="simple_adj_n")
+
+    return matcher
+
+
+class PosTagPatternMatcher(object):
+    def __init__(self, patterns, nlp, name):
+        """
+
+        :param patters: list of spacy matcher patterns
+        """
+        self.patterns = patterns
+        self.nlp = nlp
+        self.name = name
+        self.matcher = get_matcher(pattern_name=self.name, patterns=self.patterns, nlp=self.nlp)
+
+    def get_matching_spans(self, text):
+        """
+        apply pattern matcher to get spans
+        :param text:
+        :return:
+        """
+
+        sdoc = self.nlp(text)
+        matches = self.matcher(sdoc)
+        gen = (sdoc[m[1]:m[2]] for m in matches)  # create a span from each match; to match pattern in other
+        matching_spans = [s for s in gen]
+
+        return matching_spans
+        elapsed_total += time.time() - start
+
+    def get_start_end_text(self, text):
+        """
+
+        :param text:
+        :return:
+        """
+
+        spans = self.get_matching_spans(text)
+        infos = [get_start_end_text_from_span(span) for span in spans]
+
+        return infos
--- nordlys/views.py
+++ nordlys/views.py
@@ -6,13 +6,38 @@ Run this to start service. specify configs in config.yaml in this directory.
 # TODO weird import order here is artifact of trying to call attachCurrentThread at the right time. Problem still not solved see issue 2404.
 from nordlys.tagme.tagme import TagmeQueryProcessor
 from nordlys import config
+
 tqp = TagmeQueryProcessor()
 print("Loaded TQP")
+
+from nordlys.tagme.parsers import *
+import spacy
+
+nlp = spacy.load('en')
+non_wiki_parsers = [get_simple_adj_n(nlp)]
+
+
+def add_nonwiki_parser_output(text, parsers, response_dict):
+    """
+    add phrases spotted by parsers that are not internal to tagme.
+    :param response_dict: response dict built by TagmeQueryProcessor
+    :return:
+    """
+
+    name_to_mentions = dict()
+    for p in parsers:
+        info = p.get_start_end_text(text)
+        name_to_mentions[p.name] = info
+
+    # add mentions to el_cands if they are distinct and non-overlapping with those already picked up by tagme.
+    strs = set()
+    for el_cand in response_dict["el_cands"]:
+
 
 from flask import Flask, request, jsonify
 app = Flask(__name__, static_url_path='', static_folder='static')
 
-#tqp.process_query({"text": "What Is Target Yield For Mexico 10 Year Government Bond By End Of 2018"})
+
 
 @app.route('/api/tagme/proc_query/v1', methods=['GET', 'POST'])
 def proc_q():
@@ -24,6 +49,7 @@ def proc_q():
     # get qa_system
     params = dict((key, value) for key, value in request.json.iteritems())  # TODO if switch to python3 use items()
     res = tqp.process_query(params)
+
 
     return jsonify(res)
 
